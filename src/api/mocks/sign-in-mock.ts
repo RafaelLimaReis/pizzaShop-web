b50a9f8c2cbb45c7b24811e6718ec576f7f6aa5f
Rafael Reis <rafaelreis@gurgelmix.com.br>
2024-11-25 13:20:44 -0300

    primeiro mock de login

---
+++ src/api/mocks/sign-in-mock.ts
@@ -0,0 +1,18 @@
+import { http, HttpResponse } from 'msw'
+
+import { SignInBodyInterface } from '../sing-in'
+
+export const signInMock = [
+    http.post<never, SignInBodyInterface>('/authenticate', async ({ request }) => {
+        const { email } = await request.json()
+        if (email === 'johndoe@example.com') {
+            return new HttpResponse(null, {
+                status: 200,
+                headers: {
+                    'Set-Cookie': 'auth=sample-jwt',
+                },
+            })
+        }
+        return new HttpResponse(null, { status: 401 })
+    }),
+]
